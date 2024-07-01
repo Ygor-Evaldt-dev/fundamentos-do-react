5fe75f875db9d20dd7cbf40798e83d72adfb10ac
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-07-01 08:01:32 -0300

    adicionado exemplo de componentes utilizando algumas classes do tailwind css

---
+++ my-app/src/app/componentes/titulo/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <section>
+            <h1 className="text-red-500, text-6xl font-bold">Título</h1>
+            <h2 className="text-red-950, text-4xl">Subtítulo</h2>
+        </section>
+    );
+}
