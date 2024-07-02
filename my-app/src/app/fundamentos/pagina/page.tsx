9951ac767f987fe153cf3581cd71131dfdb45343
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-07-02 07:49:41 -0300

    adicionado componente Header para trabalhar os fundamentos de páginas

---
+++ my-app/src/app/fundamentos/pagina/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/Header";
+
+export default function Page() {
+    return (
+        <section className="p-2">
+            <Header />
+        </section>
+    )
+}
---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className={`
+            h-36 flex justify-center items-center
+            bg-purple-600 text-white text-3xl
+        `}>
+            <h1>Cabeçalho</h1>
+        </header>
+    );
+}
