ddd598769b760280072654925335e14aa71ad2fa
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-07-12 13:15:11 -0300

    adicionado scroll no menu lateral

--- my-app/src/components/menu/Menu.tsx
+++ my-app/src/components/menu/Menu.tsx
@@ -1,86 +1,86 @@
 import {
-	IconArrowDown,
-	IconArrowUp,
-	IconBrandJavascript,
-	IconCalculator,
-	IconCode,
-	IconFileCheck,
-	IconForms,
-	IconH1,
-	IconNumbers,
-	IconRepeat
+    IconArrowDown,
+    IconArrowUp,
+    IconBrandJavascript,
+    IconCalculator,
+    IconCode,
+    IconFileCheck,
+    IconForms,
+    IconH1,
+    IconNumbers,
+    IconRepeat
 } from '@tabler/icons-react';
 import MenuItem from './menu-item/MenuItem';
 
 export default function Menu() {
-	return (
-		<aside
-			className={`
+    return (
+        <aside
+            className={`
             min-w-52 px-2 py-5
             flex flex-col gap-1
         `}
-		>
-			<span className="text-sm text-zinc-500">Comunicação</span>
-			<MenuItem
-				label="Direta"
-				url="/comunicacao/direta"
-				icon={<IconArrowDown />}
-			/>
-			<MenuItem
-				label="Indireta"
-				url="/comunicacao/indireta"
-				icon={<IconArrowUp />}
-			/>
-			<span className="text-sm text-zinc-500">Estado</span>
-			<MenuItem
-				label="Componente sem estado"
-				url="/estado/sem"
-				icon={<IconCode />}
-			/>
-			<MenuItem
-				label="Componente com estado"
-				url="/estado/com"
-				icon={<IconCode />}
-			/>
-			<MenuItem label="Contador" url="/contador" icon={<IconNumbers />} />
-			<MenuItem
-				label="Campo de entrada"
-				url="/campo-de-entrada"
-				icon={<IconForms />}
-			/>
-			<MenuItem
-				label="Calculadora"
-				url="/calculadora"
-				icon={<IconCalculator />}
-			/>
-			<span className="text-sm text-zinc-500">Fundamentos</span>
-			<MenuItem label="Início" url="/pagina" icon={<IconFileCheck />} />
-			<MenuItem
-				label="Evento"
-				url="/evento-basico"
-				icon={<IconRepeat />}
-			/>
-			<MenuItem
-				label="Fundamentos"
-				url="/fundamentos/pagina"
-				icon={<IconCode />}
-			/>
-			<span className="text-sm text-zinc-500">Componentes</span>
-			<MenuItem
-				label="Básico"
-				url="/componentes/basico"
-				icon={<IconCode />}
-			/>
-			<MenuItem
-				label="Título"
-				url="/componentes/titulo"
-				icon={<IconH1 />}
-			/>
-			<MenuItem
-				label="Trecho"
-				url="/componentes/trecho"
-				icon={<IconBrandJavascript />}
-			/>
-		</aside>
-	);
+        >
+            <span className="text-sm text-zinc-500">Comunicação</span>
+            <MenuItem
+                label="Direta"
+                url="/comunicacao/direta"
+                icon={<IconArrowDown />}
+            />
+            <MenuItem
+                label="Indireta"
+                url="/comunicacao/indireta"
+                icon={<IconArrowUp />}
+            />
+            <span className="text-sm text-zinc-500">Estado</span>
+            <MenuItem
+                label="Componente sem estado"
+                url="/estado/sem"
+                icon={<IconCode />}
+            />
+            <MenuItem
+                label="Componente com estado"
+                url="/estado/com"
+                icon={<IconCode />}
+            />
+            <MenuItem label="Contador" url="/contador" icon={<IconNumbers />} />
+            <MenuItem
+                label="Campo de entrada"
+                url="/campo-de-entrada"
+                icon={<IconForms />}
+            />
+            <MenuItem
+                label="Calculadora"
+                url="/calculadora"
+                icon={<IconCalculator />}
+            />
+            <span className="text-sm text-zinc-500">Fundamentos</span>
+            <MenuItem label="Início" url="/pagina" icon={<IconFileCheck />} />
+            <MenuItem
+                label="Evento"
+                url="/evento-basico"
+                icon={<IconRepeat />}
+            />
+            <MenuItem
+                label="Fundamentos"
+                url="/fundamentos/pagina"
+                icon={<IconCode />}
+            />
+            <span className="text-sm text-zinc-500">Componentes</span>
+            <MenuItem
+                label="Básico"
+                url="/componentes/basico"
+                icon={<IconCode />}
+            />
+            <MenuItem
+                label="Título"
+                url="/componentes/titulo"
+                icon={<IconH1 />}
+            />
+            <MenuItem
+                label="Trecho"
+                url="/componentes/trecho"
+                icon={<IconBrandJavascript />}
+            />
+        </aside>
+    );
 }
--- my-app/src/components/side-area/SideArea.tsx
+++ my-app/src/components/side-area/SideArea.tsx
@@ -2,10 +2,10 @@ import Logo from '@/components/logo/Logo';
 import Menu from '@/components/menu/Menu';
 
 export default function SideArea() {
-	return (
-		<section className="bg-black">
-			<Logo />
-			<Menu />
-		</section>
-	);
+    return (
+        <section className="bg-black overflow-y-auto">
+            <Logo />
+            <Menu />
+        </section>
+    );
 }
